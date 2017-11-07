b4f87d136739663f662098d4affd7bf1f7e0e3f9
alliMD <allison.mejiadiestra@mail.mcgill.ca>
2017-11-07 13:53:16 -0500

    added a relocalization test

---
+++ src/ca/mcgill/ecse211/test/RelocalizationTest.java
@@ -0,0 +1,46 @@
+package ca.mcgill.ecse211.test;
+
+import ca.mcgill.ecse211.localization.Relocalization;
+import ca.mcgill.ecse211.navigation.Driver;
+import ca.mcgill.ecse211.odometry.Odometer;
+import ca.mcgill.ecse211.odometry.OdometryCorrection;
+import ca.mcgill.ecse211.robot.Robot;
+import ca.mcgill.ecse211.sensor.LightSensor;
+import ca.mcgill.ecse211.sensor.LineDetector;
+import ca.mcgill.ecse211.sensor.UltrasonicSensor;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class RelocalizationTest extends Robot{
+	
+	 private final static double BASE_WIDTH = 12.5;
+	 private final static double WHEEL_RADIUS = 2.1;
+	 private final static double SQUARE_WIDTH = 30.48;
+	 
+	 public void main(String args[]){
+		 RelocalizationTest relocalizeTest = new RelocalizationTest();
+		 relocalizeTest.run();
+	 }
+	
+	public void run(){
+		 UltrasonicSensor usSensor = new UltrasonicSensor("S1", "distance");
+		 LightSensor lightSensor = new LightSensor("S4", "red");
+		 LineDetector lineDetector = new LineDetector(lightSensor, 20, 8);
+		 
+		 EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+		 EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+		 
+		 Odometer odometer = new Odometer(leftMotor, rightMotor, WHEEL_RADIUS, BASE_WIDTH);
+		 OdometryCorrection odometryCorrection = new OdometryCorrection(odometer,lineDetector,SQUARE_WIDTH,0);
+		 
+		 Driver driver = new Driver(odometer, rightMotor, rightMotor, WHEEL_RADIUS, BASE_WIDTH);
+		 
+		 Relocalization relocalize = new Relocalization(odometer, lineDetector, driver, SQUARE_WIDTH);
+		 
+		 driver.travelTo(1 * SQUARE_WIDTH, 3 * SQUARE_WIDTH);
+		 driver.travelTo(2 * SQUARE_WIDTH, 2 * SQUARE_WIDTH);
+		 relocalize.doRandomLocalization();
+		 driver.travelTo(2 * SQUARE_WIDTH, 2 * SQUARE_WIDTH);    
+	}
+
+}
